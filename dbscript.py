b54896f3852f174430a09cda953dd5654a410f51
Robin Broda <robin@broda.me>
2019-08-11 12:30:00 +0200

    add basic argparse

---
+++ dbscript.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+
+args = parser.parse_args()
+
